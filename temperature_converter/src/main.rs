3d28bb9c9ea4f0311c0da9455b927e2d2ebf647e
Kyam Harris <kyam.lh@googlemail.com>
2020-03-05 11:48:02 +0000

    Add converse conversion

--- temperature_converter/src/main.rs
+++ temperature_converter/src/main.rs
@@ -19,11 +19,25 @@ fn main() {
         let converted = convert_farenheit_to_celcius(temperature);
         println!("{0:.2} farenheit is {1:.2} celcius", temperature, converted);
     }
+
+    println!();
+
+    for temperature in celcius_temps.iter() {
+        let converted = convert_celcius_to_farenheit(temperature);
+        println!("{0:.2} celcius is {1:.2} farenheit", temperature, converted);
+    }
 }
 
-fn convert_farenheit_to_celcius(farenheit: &f64) ->f32 {
+fn convert_farenheit_to_celcius(farenheit: &f64)->f32 {
     let old_temp = *farenheit as f32;
     let multiplier: f32 = 5.0/9.0;
     let new_temp: f32 = (old_temp - 32.0) * multiplier;
     new_temp
+}
+
+fn convert_celcius_to_farenheit(celcius: &f64)->f32 {
+    let temp = *celcius as f32;
+    let multiplier: f32 = 9.0/5.0;
+    let var: f32 = (temp * multiplier) + 32.0;
+    var
 }
