cc083089a1df24143a1eda89538189b416db2155
Kyam Harris <kyam.lh@googlemail.com>
2020-03-05 11:44:29 +0000

    Rename variables to be descriptive

--- temperature_converter/src/main.rs
+++ temperature_converter/src/main.rs
@@ -1,5 +1,5 @@
 fn main() {
-    let temps = [
+    let farenheit_temps = [
         -5.2,
         0.0,
         15.2,
@@ -13,15 +13,17 @@ fn main() {
         64.7
         ];
 
-    for temperature in temps.iter() {
+    let celcius_temps: [f64; 4] = [1.4, -6.5, 55.1, 67.2];
+
+    for temperature in farenheit_temps.iter() {
         let converted = convert_farenheit_to_celcius(temperature);
         println!("{0:.2} farenheit is {1:.2} celcius", temperature, converted);
     }
 }
 
 fn convert_farenheit_to_celcius(farenheit: &f64) ->f32 {
-    let temp = *farenheit as f32;
+    let old_temp = *farenheit as f32;
     let multiplier: f32 = 5.0/9.0;
-    let var: f32 = (temp - 32.0) * multiplier;
-    var
+    let new_temp: f32 = (old_temp - 32.0) * multiplier;
+    new_temp
 }
