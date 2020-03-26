612850c3be10bb19a818e223beb2d9a1abb06576
Kyam Harris <kyam.lh@googlemail.com>
2020-03-26 10:41:52 +0000

    Add associated function

--- rectangles/src/main.rs
+++ rectangles/src/main.rs
@@ -14,11 +14,19 @@ impl Rectangle {
     }
 }
 
+impl Rectangle {
+    fn square(size: u32) -> Rectangle {
+        Rectangle { width: size, height: size }
+    }
+}
+
 fn main() {
     let rect1 = Rectangle { width: 30, height: 50 };
     let rect2 = Rectangle { width: 10, height: 40 };
     let rect3 = Rectangle { width: 60, height: 45 };
+    let square = Rectangle::square(3);
 
     println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
     println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
+    println!("Area of square is {}", square.area());
 }
